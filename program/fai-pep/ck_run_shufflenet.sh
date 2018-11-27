a7ac6cee063502ecc4f46b408f1e923a92088ccf
Anton Lokhmotov <anton@dividiti.com>
2018-11-27 11:54:41 +0000

    Read path to model from env var.

--- program/fai-pep/ck_run_shufflenet.sh
+++ program/fai-pep/ck_run_shufflenet.sh
@@ -2,4 +2,4 @@
 
 cd ${CK_ENV_BENCH_FAI_PEP}
 
-${CK_ENV_COMPILER_PYTHON_FILE} benchmarking/run_bench.py -b specifications/models/caffe2/shufflenet/shufflenet.json
+${CK_ENV_COMPILER_PYTHON_FILE} benchmarking/run_bench.py -b ${CK_ENV_BENCH_FAI_PEP_MODEL}
