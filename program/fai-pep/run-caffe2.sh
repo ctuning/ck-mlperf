ac96a791dbe5157f763aabd70e6e249180d2531e
Anton Lokhmotov <anton@dividiti.com>
2018-11-27 16:47:47 +0000

    Use path to PyTorch source with --repo_dir.

--- program/fai-pep/run-caffe2.sh
+++ program/fai-pep/run-caffe2.sh
@@ -5,4 +5,4 @@ cd ${CK_ENV_BENCH_FAI_PEP}
 ${CK_ENV_COMPILER_PYTHON_FILE} benchmarking/run_bench.py \
   --framework "caffe2" \
   -b ${CK_ENV_BENCH_FAI_PEP_MODEL} \
-  --repo_dir ${CK_ENV_BENCH_FAI_PEP_BENCHMARKING}
+  --repo_dir ${CK_ENV_LIB_PYTORCH}/pytorch
