c69e11b62e5859ce4a70ac16e9dac837c5884541
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-03-12 18:03:35 +0000

    after the bugfix in tf2onnx, switching to --opset 8

--- package/model-onnx-mobilenet-mlperf-convert-from-tf/install.sh
+++ package/model-onnx-mobilenet-mlperf-convert-from-tf/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$CK_ENV_COMPILER_PYTHON_FILE -m tf2onnx.convert --input $CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH --inputs $MODEL_INPUT_LAYER_NAME --inputs-as-nchw $MODEL_INPUT_LAYER_NAME --outputs $MODEL_OUTPUT_LAYER_NAME --fold_const --verbose --opset 7 --output ${INSTALL_DIR}/${PACKAGE_NAME}
+$CK_ENV_COMPILER_PYTHON_FILE -m tf2onnx.convert --input $CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH --inputs $MODEL_INPUT_LAYER_NAME --inputs-as-nchw $MODEL_INPUT_LAYER_NAME --outputs $MODEL_OUTPUT_LAYER_NAME --fold_const --verbose --opset 8 --output ${INSTALL_DIR}/${PACKAGE_NAME}
