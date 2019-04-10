ce6666719dd51ae79b8520fa271f40f7ef385a66
Anton Lokhmotov <anton@dividiti.com>
2019-04-10 13:13:14 +0100

    Rename tf_2_tflite to tf2tflite.

--- package/model-tflite-convert-from-tf/install.sh
+++ package/model-tflite-convert-from-tf/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$CK_ENV_COMPILER_PYTHON_FILE $PACKAGE_DIR/tf_2_tflite.py
+$CK_ENV_COMPILER_PYTHON_FILE $PACKAGE_DIR/tf2tflite.py
--- package/model-tflite-convert-from-tf/tf2tflite.py
+++ package/model-tflite-convert-from-tf/tf2tflite.py
@@ -2,8 +2,8 @@
 
 import tensorflow as tf
 
-def tf_2_tflite(input_ft_model_filepath, output_tflite_model_filepath, input_layer_names, output_layer_names):
-    "Perform the actual conversion from TF to TFlite"
+def tf2tflite(input_ft_model_filepath, output_tflite_model_filepath, input_layer_names, output_layer_names):
+    "Convert from TF to TFLite"
 
     converter = tf.contrib.lite.TFLiteConverter.from_frozen_graph(input_tf_model_filepath, input_layer_names, output_layer_names)
     tflite_model = converter.convert()
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     input_tf_model_filepath         = os.environ['CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH']
     output_tflite_model_filepath    = os.path.join(os.environ['INSTALL_DIR'], os.environ['PACKAGE_NAME'])
 
-    # FIXME: ask the TF model what input and output layers it supports
+    # FIXME: ask the TF model about its input and output layers.
     #
     input_layer_names = [ os.environ['MODEL_INPUT_LAYER_NAME'] ]
     output_layer_names = [ os.environ['MODEL_OUTPUT_LAYER_NAME'] ]
 
-    tf_2_tflite(input_tf_model_filepath, output_tflite_model_filepath, input_layer_names, output_layer_names)
+    tf2tflite(input_tf_model_filepath, output_tflite_model_filepath, input_layer_names, output_layer_names)
