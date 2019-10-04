90c6b0467138d812485b77f21fcaa591a4506f39
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-10-04 15:16:10 +0100

    schindler.py also supports CK_LOADGEN_DATASET_SIZE, even if 0

--- script/image-classification/schindler.py
+++ script/image-classification/schindler.py
@@ -10,6 +10,7 @@ import os
 
 def ck_preprocess(i):
     print('\n-=-=-=-=-= Generating a list of files to be processed...')
+    def has_env(var): return var in i['env']
     def my_env(var): return i['env'].get(var)
     def dep_env(dep, var): return i['deps'][dep]['dict']['env'].get(var)
     def has_dep_env(dep, var): return var in i['deps'][dep]['dict']['env']
@@ -17,12 +18,13 @@ def ck_preprocess(i):
     image_list_filename = dep_env('images', 'CK_ENV_DATASET_IMAGENET_PREPROCESSED_SUBSET_FOF')
     source_dir          = dep_env('images', 'CK_ENV_DATASET_IMAGENET_PREPROCESSED_DIR')
     preprocessed_ext    = dep_env('images', 'CK_ENV_DATASET_IMAGENET_PREPROCESSED_NEW_EXTENSION')
-    image_count         = int(my_env('CK_BATCH_SIZE')) * int(my_env('CK_BATCH_COUNT'))
+
+    image_count         = int(my_env('CK_LOADGEN_DATASET_SIZE')) if has_env('CK_LOADGEN_DATASET_SIZE') else int(my_env('CK_BATCH_SIZE')) * int(my_env('CK_BATCH_COUNT'))
     images_offset       = int(my_env('CK_SKIP_IMAGES') or '0')
 
     sorted_filenames    = [filename for filename in sorted(os.listdir(source_dir)) if filename.lower().endswith('.' + preprocessed_ext) ]
 
-    selected_filenames  = sorted_filenames[images_offset:images_offset+image_count]
+    selected_filenames  = sorted_filenames[images_offset:images_offset+image_count] if image_count else sorted_filenames[images_offset:]
 
     selected_var_paths  = [ os.path.join("$<<CK_ENV_DATASET_IMAGENET_PREPROCESSED_DIR>>$", filename) for filename in selected_filenames ]
 
