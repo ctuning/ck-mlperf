03d0503644a044c9707f4471f7c415252f486ece
Anton Lokhmotov <anton@dividiti.com>
2019-10-04 23:53:13 +0100

    Use --tags=mobilenet,non-quantized for mobilenet.

--- script/mlperf.closed.image-classification/accuracy.sh
+++ script/mlperf.closed.image-classification/accuracy.sh
@@ -21,23 +21,26 @@ buffer_size=500
 
 # Image classification models (in the closed division).
 models=( 'mobilenet' 'resnet' )
-# Preprocessing methods.
-methods=( 'preprocessing,using-opencv' 'preprocessing,using-tensorflow' )
+models_tags=( 'mobilenet,non-quantized' 'resnet' )
+# Preferred preprocessing methods per model.
+preprocessing_methods=( 'preprocessed,using-opencv' 'preprocessed,using-tensorflow' )
+# Iterate for each model.
 for i in $(seq 1 ${#models[@]}); do
   model=${models[${i}-1]}
-  method=${methods[0]} # use the same preprocessing for any model
+  model_tags=${models_tags[${i}-1]}
+  preprocessing_method=${preprocessing_methods[0]} # use the same method for all models
   echo "Benchmarking '${model}' ..."
-  echo ck benchmark program:image-classification-tflite-loadgen \
+  ck benchmark program:image-classification-tflite-loadgen \
   --speed --repetitions=1 \
   --env.CK_LOADGEN_SCENARIO=${scenario} \
   --env.CK_LOADGEN_MODE=${mode} \
   --env.CK_LOADGEN_DATASET_SIZE=${dataset_size} \
   --env.CK_LOADGEN_BUFFER_SIZE=${buffer_size} \
   --env.CK_VERBOSE=2 \
-  --dep_add_tags.weights=${model} \
-  --dep_add_tags.images=${method} \
+  --dep_add_tags.weights=${model_tags} \
   --dep_add_tags.library=${library} \
   --dep_add_tags.compiler=${compiler} \
+  --dep_add_tags.images=${preprocessing_method} \
   --record --record_repo=local \
   --record_uoa=mlperf.${division}.${task}.${system}.${model}.${scenario_tag}.${mode_tag}.${dataset_size} \
   --tags=mlperf,${division},${task},${system},${model},${scenario_tag},${mode_tag},${dataset_size} \
