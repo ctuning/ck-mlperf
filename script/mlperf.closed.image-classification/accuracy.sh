55688738027f523255657e32e5253b05a18add67
Anton Lokhmotov <anton@dividiti.com>
2019-10-05 12:37:47 +0100

    Update mlperf.closed.image-classification/accuracy.sh.

--- script/mlperf.closed.image-classification/accuracy.sh
+++ script/mlperf.closed.image-classification/accuracy.sh
@@ -2,55 +2,74 @@
 
 division='closed'
 task='image-classification'
-
-system='hikey960'
-library='tflite,v1.15'
-if [ ${system} = 'rpi4' ]
-then
-  compiler='gcc,v8'
-else
-  compiler='gcc,v7'
-fi
+imagenet_size=50000
 
 scenario='SingleStream'
 scenario_tag='singlestream'
+
+hostname=`hostname`
+if [ ${hostname} = 'hikey961' ]
+then
+  system='hikey960'
+else
+  system=${hostname}
+fi
+
+if [ ${system} = 'hikey960' ] || [ ${system} = 'firefly' ]
+then
+  compiler='gcc,v7'
+else
+  compiler='gcc,v8'
+fi
 
 mode='AccuracyOnly'
 mode_tag='accuracy'
 if [ ${mode_tag} = 'accuracy' ]
 then
-  dataset_size=5000 # FIXME: 50000
+  dataset_size=50000
   buffer_size=500
 else
   dataset_size=1024
   buffer_size=1024
 fi
 
+# Library.
+library='tflite,v1.15'
+library_tag='tflite-v1.15'
+
 # Image classification models (in the closed division).
 models=( 'mobilenet' 'resnet' )
 models_tags=( 'mobilenet,non-quantized' 'resnet' )
 # Preferred preprocessing methods per model.
 preprocessing_methods=( 'preprocessed,using-opencv' 'preprocessed,using-tensorflow' )
+
 # Iterate for each model.
 for i in $(seq 1 ${#models[@]}); do
+  # Configure for the model.
   model=${models[${i}-1]}
   model_tags=${models_tags[${i}-1]}
-  preprocessing_method=${preprocessing_methods[0]} # use the same method for all models
+  # Use the same preprocessing method for all models.
+  preprocessing_method=${preprocessing_methods[0]}
+  # Configure record settings.
+  record_uoa="mlperf.${division}.${task}.${system}.${library_tag}.${model}.${scenario_tag}.${mode_tag}"
+  record_tags="mlperf,${division},${task},${system},${library_tag},${model},${scenario_tag},${mode_tag}"
+  if [ ${mode} = 'accuracy' ] && [ "${dataset_size}" != "${imagenet_size}" ]; then
+    record_uoa+=".${dataset_size}"
+    record_tags+=",${dataset_size}"
+  fi
+  # Benchmark.
   echo "Benchmarking '${model}' ..."
-  ck benchmark program:image-classification-tflite-loadgen \
-  --speed --repetitions=1 \
+  echo ck benchmark program:image-classification-tflite-loadgen \
+  --speed --repetitions=1 --env.CK_VERBOSE=2 \
   --env.CK_LOADGEN_SCENARIO=${scenario} \
   --env.CK_LOADGEN_MODE=${mode} \
   --env.CK_LOADGEN_DATASET_SIZE=${dataset_size} \
   --env.CK_LOADGEN_BUFFER_SIZE=${buffer_size} \
-  --env.CK_VERBOSE=2 \
   --dep_add_tags.weights=${model_tags} \
   --dep_add_tags.library=${library} \
   --dep_add_tags.compiler=${compiler} \
   --dep_add_tags.images=${preprocessing_method} \
-  --record --record_repo=local \
-  --record_uoa=mlperf.${division}.${task}.${system}.${model}.${scenario_tag}.${mode_tag}.${dataset_size} \
-  --tags=mlperf,${division},${task},${system},${model},${scenario_tag},${mode_tag},${dataset_size} \
+  --record --record_repo=local --record_uoa=${record_uoa} --tags=${record_tags} \
   --skip_print_timers --skip_stat_analysis --process_multi_keys
   echo
 done # for each model
