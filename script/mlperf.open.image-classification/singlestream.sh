c1bd2e2855f64d19e77f146a55050bc92aacd2d8
Anton Lokhmotov <anton@dividiti.com>
2019-10-07 15:53:19 +0100

    Generate mobilenet variations.

--- script/mlperf.open.image-classification/singlestream.sh
+++ script/mlperf.open.image-classification/singlestream.sh
@@ -35,7 +35,7 @@ multipliers=( 1.0 0.75 0.5 0.25 )
 for resolution in ${resolutions[@]}; do
   for multiplier in ${multipliers[@]}; do
     models+=( "mobilenet-v1-${multiplier}-${resolution}" )
-    models_tags+=( "model,tflite,mobilenet-v1-${multiplier}-${resolution},non-quantized" )
+    models_tags+=( "model,tflite,mobilenet,v1-${multiplier}-${resolution},non-quantized" )
     preprocessing_tags_list+=( "side.${resolution},preprocessed,using-opencv" )
   done
 done
