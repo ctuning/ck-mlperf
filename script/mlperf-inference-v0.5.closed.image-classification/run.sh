e42d19f37ffce1bb0bc52978c092ec8abee452ab
Anton Lokhmotov <anton@dividiti.com>
2019-10-09 15:33:55 +0000

    Support closed with ArmNN-TFLite with Neon backend.

--- script/mlperf-inference-v0.5.closed.image-classification/run.sh
+++ script/mlperf-inference-v0.5.closed.image-classification/run.sh
@@ -7,6 +7,15 @@ imagenet_size=50000
 scenario="singlestream"
 scenario_tag="SingleStream"
 
+implementation_tflite="image-classification-tflite-loadgen"
+implementation_armnn="image-classification-armnn-tflite-loadgen"
+implementation_armnn_backend_neon="neon"
+implementation_armnn_backend_opencl="opencl"
+
+# TODO: Add iteration over implementations and backends.
+implementation="${implementation_armnn}"
+implementation_armnn_backend=${implementation_armnn_backend_neon}
+
 # System, library.
 hostname=`hostname`
 if [ "${hostname}" = "diviniti" ]; then
@@ -21,26 +30,42 @@ else
   android=""
 fi
 
-# Library. NB: Currently, we only support TFLite v1.13 for Android.
-if [ "${android}" != "" ]; then
-  library="tflite-v1.13"
-  library_tags="tflite,v1.13"
+# Library.
+if [ "${implementation}" == "image-classification-tflite-loadgen" ]; then
+  if [ "${android}" != "" ]; then
+    # NB: Currently, we only support TFLite v1.13 for Android.
+    library="tflite-v1.13"
+    library_tags="tflite,v1.13"
+  else
+    library="tflite-v1.15"
+    library_tags="tflite,v1.15"
+  fi
+  armnn_backend=""
+elif [ "${implementation}" == ${implementation_armnn} ]; then
+  library="armnn-v19.08"
+  library_tags="armnn,tflite,neon,opencl,rel.19.08"
+  if [ "${implementation_armnn_backend}" == "${implementation_armnn_backend_opencl}" ]; then
+    armnn_backend="--env.USE_OPENCL=1"
+  elif [ "${implementation_armnn_backend}" == "${implementation_armnn_backend_neon}" ]; then
+    armnn_backend="--env.USE_NEON=1"
+  else
+    echo "ERROR: Unsupported ArmNN backend '${implementation_armnn_backend}'!"
+    exit 1
+  fi
 else
-  library="tflite-v1.15"
-  library_tags="tflite,v1.15"
+  echo "ERROR: Unsupported implementation '${implementation}'!"
+  exit 1
 fi
 
-# Compiler. NB: Currently, we only support Clang 6 (NDK 17c) for Android.
+# Compiler.
 if [ "${system}" = "mate10pro" ]; then
+  # NB: Currently, we only support Clang 6 (NDK 17c) for Android.
   compiler_tags="llvm,v6"
 elif [ "${system}" = "hikey960" ] || [ "${system}" = "firefly" ]; then
   compiler_tags="gcc,v7"
 else
   compiler_tags="gcc,v8"
 fi
-
-# Implementation.
-implementation="image-classification-tflite-loadgen"
 
 # Image classification models (in the closed division).
 models=( "mobilenet" "resnet" )
@@ -81,8 +106,14 @@ for i in $(seq 1 ${#models[@]}); do
     # Opportunity to skip by mode or model.
     #if [ "${mode}" != "performance" ] || [ "${model}" != "mobilenet" ]; then continue; fi
     # Configure record settings.
-    record_uoa="mlperf.${division}.${task}.${system}.${library}.${model}.${scenario}.${mode}"
-    record_tags="mlperf,${division},${task},${system},${library},${model},${scenario},${mode}"
+    record_uoa="mlperf.${division}.${task}.${system}.${library}"
+    record_tags="mlperf,${division},${task},${system},${library}"
+    if [ "${implementation}" == "${implementation_armnn}" ]; then
+      record_uoa+=".${implementation_armnn_backend}"
+      record_tags+=",${implementation_armnn_backend}"
+    fi
+    record_uoa+=".${model}.${scenario}.${mode}"
+    record_tags+=",${model},${scenario},${mode}"
     if [ "${mode}" = "accuracy" ]; then
       # Get substring after "preprocessed," to end.
       preprocessing="${model_preprocessing_tags##*preprocessed,}"
@@ -97,7 +128,7 @@ for i in $(seq 1 ${#models[@]}); do
     echo "Running '${model}' in '${mode}' mode ..."
     read -d '' CMD <<END_OF_CMD
     ck benchmark program:${implementation} \
-    --speed --repetitions=1 ${android} \
+    --speed --repetitions=1 ${android} ${armnn_backend} \
     --env.CK_VERBOSE=${verbose} \
     --env.CK_LOADGEN_SCENARIO=${scenario_tag} \
     --env.CK_LOADGEN_MODE=${mode_tag} \
@@ -116,7 +147,7 @@ END_OF_CMD
     echo
     # Check for errors.
     if [ "${?}" != "0" ]; then
-      echo "Error: Failed running '${model}' in '${mode}' mode ..."
+      echo "ERROR: Failed running '${model}' in '${mode}' mode!"
       exit 1
     fi
   done # for each mode
