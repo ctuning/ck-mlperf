eb6731888c8dde4c4d27022556684cfd5a4bc9cd
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-10-17 12:42:56 +0100

    batches are no longer supported

--- script/mlperf-inference-v0.5.open.object-detection/run.sh
+++ script/mlperf-inference-v0.5.open.object-detection/run.sh
@@ -45,16 +45,20 @@ if [ -n "$CK_QUICK_TEST" ]; then
     scenarios=( 'SingleStream' )
     scenarios_lowercase=( 'singlestream' )
 
-    modes=( 'AccuracyOnly' )
-    modes_lowercase=( 'accuracy' )
-    modes_selection=( "--env.CK_ACCURACY=YES" )
+    #modes=( 'AccuracyOnly' )
+    #modes_lowercase=( 'accuracy' )
+    #modes_selection=( "--env.CK_LOADGEN_MODE='--accuracy'" )
+
+    modes=( 'PerformanceOnly' )
+    modes_lowercase=( 'performance' )
+    modes_selection=( "--env.CK_LOADGEN_MODE=''" )
 else
     scenarios=( 'SingleStream' 'Offline' )
     scenarios_lowercase=( 'singlestream' 'offline' )
 
     modes=( 'AccuracyOnly' 'PerformanceOnly' )
     modes_lowercase=( 'accuracy' 'performance' )
-    modes_selection=( "--env.CK_ACCURACY=YES" "--env.CK_ACCURACY=NO" )
+    modes_selection=( "--env.CK_LOADGEN_MODE='--accuracy'" "--env.CK_LOADGEN_MODE=''" )
 fi
 
 mode=${modes[0]}
@@ -63,15 +67,15 @@ mode_selection=${modes_selection[0]}
 
 scenarios_selection=()
 for scenario in "${scenarios[@]}"; do
-  scenarios_selection+=( "--env.CK_SCENARIO=${scenario}" )
+  scenarios_selection+=( "--env.CK_LOADGEN_SCENARIO=${scenario}" )
 done
 
 # FIXME: Invalid batch sizes and counts.
 # NB: batch_sizes (#samples/query) must be 1 for SingleStream.
 # NB: batch_count (#queries) must be at least 1024 for SingleStream.
 # batch_sizes=( 1 2 4 8 16 32 )
-batch_sizes=( 1 1 )
-batch_count=2
+#batch_sizes=( 1 1 )
+#batch_count=2
 
 
 ###############################################################################
@@ -83,6 +87,8 @@ batch_count=2
 if [ -n "$CK_QUICK_TEST" ]; then
     models=( 'yolo-v3' )
     models_tags=( 'yolo-v3' )
+    #models=( 'rcnn,inception-v2' )
+    #models_tags=( 'rcnn-inception-v2' )
 else
     models=( 'rcnn,nas,lowproposals,vcoco' 'rcnn,resnet50,lowproposals' 'rcnn,resnet101,lowproposals' 'rcnn,inception-resnet-v2,lowproposals' 'rcnn,inception-v2' 'ssd,inception-v2' 'ssd,mobilenet-v1,quantized,mlperf,tf' 'ssd,mobilenet-v1,mlperf,non-quantized,tf' 'ssd,mobilenet-v1,fpn' 'ssd,resnet50,fpn' 'ssdlite,mobilenet-v2,vcoco' 'yolo-v3' )
     models_tags=( 'rcnn-nas-lowproposals'  'rcnn-resnet50-lowproposals' 'rcnn-resnet101-lowproposals' 'rcnn-inception-resnet-v2-lowproposals' 'rcnn-inception-v2' 'ssd-inception-v2' 'ssd-mobilenet-v1-quantized-mlperf'    'ssd-mobilenet-v1-non-quantized-mlperf'    'ssd-mobilenet-v1-fpn' 'ssd-resnet50-fpn' 'ssdlite-mobilenet-v2'       'yolo-v3' )
@@ -106,10 +112,10 @@ done
 ###############################################################################
 
 if [ -n "$CK_QUICK_TEST" ]; then
-    backends_selection=( '--dep_add_tags.lib-tensorflow=vcuda --env.CK_TF_GPU_MEMORY_PERCENT=99 --env.CK_ENABLE_TENSORRT=1 --env.CK_TENSORRT_DYNAMIC=1' )
+    backends_selection=( '--dep_add_tags.lib-tensorflow=vcuda --env.CK_LOADGEN_BACKEND=tensorflowRT --env.CK_LOADGEN_TENSORRT_DYNAMIC=1' )
     backends_tags=( 'tensorrt-dynamic' )
 else
-    backends_selection=( '--dep_add_tags.lib-tensorflow=vcuda --env.CUDA_VISIBLE_DEVICES=-1' '--dep_add_tags.lib-tensorflow=vcuda --env.CK_TF_GPU_MEMORY_PERCENT=99' '--dep_add_tags.lib-tensorflow=vcuda --env.CK_TF_GPU_MEMORY_PERCENT=99 --env.CK_ENABLE_TENSORRT=1' '--dep_add_tags.lib-tensorflow=vcuda --env.CK_TF_GPU_MEMORY_PERCENT=99 --env.CK_ENABLE_TENSORRT=1 --env.CK_TENSORRT_DYNAMIC=1' )
+    backends_selection=( '--dep_add_tags.lib-tensorflow=vcuda --env.CUDA_VISIBLE_DEVICES=-1 --env.CK_LOADGEN_BACKEND=tensorflow' '--dep_add_tags.lib-tensorflow=vcuda --env.CK_LOADGEN_BACKEND=tensorflow' '--dep_add_tags.lib-tensorflow=vcuda --env.CK_LOADGEN_BACKEND=tensorflowRT' '--dep_add_tags.lib-tensorflow=vcuda --env.CK_LOADGEN_BACKEND=tensorflowRT --env.CK_LOADGEN_TENSORRT_DYNAMIC=1' )
     backends_tags=( 'cpu' 'cuda' 'tensorrt' 'tensorrt-dynamic' )
 fi
 
@@ -118,7 +124,7 @@ fi
 # 5. Full design space exploration.
 ###############################################################################
 scenarios_len=${#scenarios[@]}
-batch_sizes_len=${#batch_sizes[@]}
+#batch_sizes_len=${#batch_sizes[@]}
 backends_len=${#backends_selection[@]}
 models_len=${#models[@]}
 
@@ -130,14 +136,15 @@ for i in $(seq 1 ${scenarios_len}); do
   scenario=${scenarios[$i-1]}
   scenario_lowercase=${scenarios_lowercase[$i-1]}
   scenario_selection=${scenarios_selection[$i-1]}
-  batch_size=${batch_sizes[$i-1]}
-  if [ ${batch_size} = 1 ]
-  then
-    enable_batch=0
-  else
-    enable_batch=1
-  fi
-  batch_selection="--env.CK_ENABLE_BATCH=${enable_batch} --env.CK_BATCH_SIZE=${batch_size} --env.CK_BATCH_COUNT=${batch_count}"
+  #batch_size=${batch_sizes[$i-1]}
+  #if [ ${batch_size} = 1 ]
+  #then
+  #  enable_batch=0
+  #else
+  #  enable_batch=1
+  #fi
+  #batch_selection="--env.CK_ENABLE_BATCH=${enable_batch} --env.CK_BATCH_SIZE=${batch_size} --env.CK_BATCH_COUNT=${batch_count}"
+  batch_selection=""
   for j in $(seq 1 ${backends_len}); do
     backend_selection=${backends_selection[$j-1]}
     backend_tags=${backends_tags[$j-1]}
@@ -163,14 +170,14 @@ for i in $(seq 1 ${scenarios_len}); do
           profile='default_tf_object_det_zoo' # FIXME: tf_zoo?
         fi
       fi
-      profile_selection="--env.CK_PROFILE=${profile}"
+      profile_selection="--env.CK_LOADGEN_REF_PROFILE=${profile}"
       # Record.
       record_uoa="mlperf.${division}.${task}.${system}.${backend_tags}.${model_tags}.${scenario_lowercase}.${mode_lowercase}"
       record_tags="mlperf,${division},${task},${system},${backend_tags},${model_tags},${scenario_lowercase},${mode_lowercase}"
-      if [ ${enable_batch} = 1 ]; then
-        record_uoa+=".batch-size${batch_size}"
-        record_tags+=",batch-size${batch_size}"
-      fi
+      #if [ ${enable_batch} = 1 ]; then
+      #  record_uoa+=".batch-size${batch_size}"
+      #  record_tags+=",batch-size${batch_size}"
+      #fi
       # Print all parameters.
       echo "experiment_idx: ${experiment_idx}"
       echo "  backend_tags: ${backend_tags}"
@@ -184,8 +191,8 @@ for i in $(seq 1 ${scenarios_len}); do
       echo "  mode: ${mode}"
       echo "  mode_lowercase: ${mode_lowercase}"
       echo "  mode_selection: ${mode_selection}"
-      echo "  batch_size: ${batch_size}"
-      echo "  batch_selection: ${batch_selection}"
+      #echo "  batch_size: ${batch_size}"
+      #echo "  batch_selection: ${batch_selection}"
       echo "  profile: ${profile}"
       echo "  profile_selection: ${profile_selection}"
       echo "  record_uoa=${record_uoa}"
@@ -197,9 +204,9 @@ for i in $(seq 1 ${scenarios_len}); do
       --env-file ${CK_REPOS}/ck-mlperf/docker/${IMAGE}/env.list \
       --volume ${EXPERIMENTS_DIR}:/home/dvdt/CK_REPOS/local/experiment \
       --rm ctuning/${IMAGE} \
-        "ck benchmark program:mlperf-inference-vision --repetitions=1 --env.CK_METRIC_TYPE=COCO \
+        "ck benchmark program:mlperf-inference-vision --cmd_key=direct --repetitions=1 --env.CK_METRIC_TYPE=COCO \
         ${scenario_selection} ${mode_selection} ${batch_selection} ${model_selection} ${backend_selection} ${profile_selection} \
-        --record --record_repo=local --record_uoa=${record_uoa} --tags=${record_tags}"
+        --record --record_repo=local --record_uoa=${record_uoa} --tags=${record_tags} --env.CK_LOADGEN_EXTRA_PARAMS='$EXTRA_PARAMS'"
 END_OF_CMD
       echo ${CMD}
 
