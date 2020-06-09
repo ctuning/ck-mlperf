43a9861672253fa947b68135ff1ffb6b61391bf0
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-06-09 10:16:00 +0100

    bugfix: typo

--- soft/lib.python.coco-helper/coco_helper/__init__.py
+++ soft/lib.python.coco-helper/coco_helper/__init__.py
@@ -58,7 +58,7 @@ if GIVEN_CHANNEL_MEANS:
 
 GIVEN_CHANNEL_STDS      = os.getenv('ML_MODEL_GIVEN_CHANNEL_STDS', '')
 if GIVEN_CHANNEL_STDS:
-    GIVEN_CHANNEL_STDS = np.array(GIVEN_CHANNEL_STDS.split(' '), dtype=VECTOR_DATA_TYPE)
+    GIVEN_CHANNEL_STDS = np.fromstring(GIVEN_CHANNEL_STDS, dtype=np.float32, sep=' ').astype(INTERMEDIATE_DATA_TYPE)
     if MODEL_COLOURS_BGR:
         GIVEN_CHANNEL_STDS  = GIVEN_CHANNEL_STDS[::-1]      # swapping Red and Blue colour channels
 
