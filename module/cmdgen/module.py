0d715aa9858005b58c20a1a6f71fa3e840c115cc
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-08-24 17:46:18 +0100

    integrated iterate() into gen&run commands

--- module/cmdgen/module.py
+++ module/cmdgen/module.py
@@ -21,38 +21,68 @@ def init(i):
 
 
 def iterate(i):
+    """
+    Input:  {
+                (any_params)        - any command line parameters, some of which (being iterable) given as lists/ranges
 
+                    Please note the expected command line syntax for iterable values:
+
+                --alpha=alphaval1   - a single value
+                --beta:-5:6         - a range of integer values
+                --gamma,=10,20,30   - a list of values (comma-separated)
+                --delta:=11,22,33   - another list of values (semicolon-separated)
+            }
+
+    Output: {
+                return              - return code =  0, if successful
+                                                  >  0, if error
+                (error)             - error text if return > 0
+            }
+    Test:
+            ck iterate cmdgen --alpha,=11,22,33 --beta:-9:-7 --delta=single --gamma,=one,two,three
+            ck iterate cmdgen --alpha,=11,22,33 --beta:-9:-7 --delta=single --gamma,=one,two,three --out=json
+    """
     import re
 
-    input_params = i.copy()
+    interactive     = i.get('out')=='con'
+
+    input_params    = i.copy()
     for unwanted_key in ('action', 'repo_uoa', 'module_uoa', 'data_uoa', 'cid', 'cids', 'xcids', 'out'):
         input_params.pop(unwanted_key, None)
 
-    index_name = []
+    index_name  = []
     index_range = []
 
     for param_name in input_params.keys():
         param_value = input_params[param_name]
-        matchObj = re.match('(\w+)([,:])((-?\d+):(-?\d+))?', param_name)
+        matchObj = re.match('(\w+)([,:])?((-?\d+):(-?\d+))?', param_name)
         if matchObj:
             pure_name   = matchObj.group(1)
-            delimiter   = matchObj.group(2)
             index_name.append( pure_name )
-            if matchObj.group(3):
+            if matchObj.group(2)==None:
+                index_range.append( [param_value] )
+            elif matchObj.group(3):
                 range_from  = int(matchObj.group(4))
                 range_to    = int(matchObj.group(5))
                 index_range.append( range(range_from, range_to+1) )
             else:
+                delimiter   = matchObj.group(2)
                 index_range.append( param_value.split(delimiter) )
-    print(dict(zip(index_name, index_range)))
-    print('-'*80)
+
+    if interactive:
+        print(dict(zip(index_name, index_range)))
+        print('-'*80)
 
     dimensions  = len(index_name)
     multi_idx   = [0] * dimensions
     current_dim = dimensions-1
+    param_dicts = []
     while True:
-        multi_value = [(index_name[i], index_range[i][multi_idx[i]]) for i in range(dimensions) ]
-        print("{} -> {}".format(multi_idx, multi_value))
+        multi_value = {index_name[i]: index_range[i][multi_idx[i]] for i in range(dimensions) }
+        param_dicts.append( multi_value )
+
+        if interactive:
+            print(multi_value)
 
         multi_idx[current_dim] += 1
         # carry avalanche:
@@ -67,7 +97,7 @@ def iterate(i):
         else:
             break
 
-    return {'return':0}
+    return {'return':0, 'param_dicts': param_dicts}
 
 
 def gen(i):
@@ -75,6 +105,13 @@ def gen(i):
     Input:  {
                 data_uoa            - specific entry that contains recipe for building the command
                 (any_map_params)    - any command line parameters (supported by build_map)
+
+                    Please note the expected command line syntax for iterable values:
+
+                --alpha=alphaval1   - a single value
+                --beta:-5:6         - a range of integer values
+                --gamma,=10,20,30   - a list of values (comma-separated)
+                --delta:=11,22,33   - another list of values (semicolon-separated)
             }
 
     Output: {
@@ -85,16 +122,13 @@ def gen(i):
     Test:
             ck gen cmdgen:ls
             ck gen cmdgen:ls --mode=long --home
+            ck gen cmdgen:ls --mode,=long,short,single
     """
 
     import re
     from pprint import pprint
 
     interactive     = i.get('out')=='con'
-
-    input_params = i.copy()
-    for unwanted_key in ('action', 'repo_uoa', 'module_uoa', 'data_uoa', 'cid', 'cids', 'xcids', 'out'):
-        input_params.pop(unwanted_key, None)
 
     load_adict = {  'action':           'load',
                     'repo_uoa':         i.get('repo_uoa', ''),
@@ -105,82 +139,93 @@ def gen(i):
     if r['return']>0: return r
 
     entry_dict  = r['dict']
+    build_map   = entry_dict.get('build_map', {})
     if interactive:
         print("Entry contents:")
         pprint(entry_dict)
         print("")
 
-    build_map   = entry_dict.get('build_map', {})
-    accu        = entry_dict.get('accu_init', {})
+    i['action'] = 'iterate'
+    i['out']    = ''
+    r=ck.access( i )
+    if r['return']>0: return r
+    param_dicts = r['param_dicts']
 
-    # Accumulating values:
-    #
-    for param_name in input_params:
-        param_value = input_params[param_name]
+    cmds = []
+    for input_params in param_dicts:
 
-        if param_name in build_map:
-            specific_accu_map = build_map[param_name]
-            # Start with the specific value, but fallback to default:
-            accu_map = specific_accu_map.get(param_value) or specific_accu_map.get('###')
-            if accu_map==None:
-                return {'return':1, 'error':"build_map[{}] is missing both '{}' and '###' values".format(param_name, param_value)}
+        # Accumulating values:
+        #
+        accu        = entry_dict.get('accu_init', {}).copy()
+        for param_name in input_params:
+            param_value = input_params[param_name]
 
-            for accu_name in accu_map:
-                if accu_name not in accu:
-                    accu[accu_name] = []    # manual vivification
+            if param_name in build_map:
+                specific_accu_map = build_map[param_name]
+                # Start with the specific value, but fallback to default:
+                accu_map = specific_accu_map.get(param_value) or specific_accu_map.get('###')
+                if accu_map==None:
+                    return {'return':1, 'error':"build_map[{}] is missing both '{}' and '###' values".format(param_name, param_value)}
 
-                accu_value_list = accu_map[accu_name]
-                if type(accu_value_list)!=list:
-                    accu_value_list = [ accu_value_list ]
+                for accu_name in accu_map:
+                    if accu_name not in accu:
+                        accu[accu_name] = []    # manual vivification
 
-                for accu_value in accu_value_list:
-                    accu[accu_name].append( accu_value.replace('###', param_value) )
-
-    if interactive:
-        print("Accu contents:")
-        pprint(accu)
-        print("")
-
-    # Substitute the accumulated values into command template:
-    #
-    anchor_regexpr  = '(<<<(\??)(\w+)(.?)>>>)'
-    subst_output    = entry_dict['cmd_template']
-    can_substitute  = bool( re.search(anchor_regexpr, subst_output) )
-    iteration       = 0
-
-    while can_substitute:
-        subst_input = subst_output
-        for match in re.finditer(anchor_regexpr, subst_input):
-            expression, optional, anchor_name, accu_sep = match.group(1), match.group(2), match.group(3), match.group(4)
-            if anchor_name in accu:
-                if anchor_name in input_params:
-                    return {'return':1, 'error':"Both input_params and accu contain '{}' anchor, ambiguous substitution".format(anchor_name)}
-                else:
-                    # print("Substituting {} -> {} from accu".format(expression, accu_sep.join(accu[anchor_name])))
-                    accu_value_list = accu[anchor_name]
+                    accu_value_list = accu_map[accu_name]
                     if type(accu_value_list)!=list:
                         accu_value_list = [ accu_value_list ]
-                    subst_output = subst_output.replace(expression, accu_sep.join(accu_value_list) )
-            elif anchor_name in input_params:
-                # print("Substituting {} -> {} from input_params".format(expression, accu_sep.join(input_params[anchor_name])))
-                subst_output = subst_output.replace(expression, accu_sep.join(input_params[anchor_name]) )
-            elif optional=='?':
-                # print("Substituting optional {} -> ''".format(expression))
-                subst_output = subst_output.replace(expression, '')
-            else:
-                return {'return':1, 'error':"Neither input_params nor accu contain substitution for non-optional '{}' anchor".format(anchor_name)}
 
-            # print("input={}\noutput={}\n".format(subst_input, subst_output))
+                    for accu_value in accu_value_list:
+                        accu[accu_name].append( accu_value.replace('###', param_value) )
 
-        can_substitute  = bool( re.search(anchor_regexpr, subst_output) )
         if interactive:
-            iteration += 1
-            print("Substitution iteration #{}".format(iteration))
+            print('-'*80)
+            print("Accu contents:")
+            pprint(accu)
+            print("")
 
-    if interactive:
-        print("The generated command:\n  {}".format(subst_output.replace(' --', ' \\\n    --')))
+        # Substitute the accumulated values into command template:
+        #
+        anchor_regexpr  = '(<<<(\??)(\w+)(.?)>>>)'
+        subst_output    = entry_dict['cmd_template']
+        can_substitute  = bool( re.search(anchor_regexpr, subst_output) )
+        iteration       = 0
 
-    return { 'return': 0, 'cmd': subst_output }
+        while can_substitute:
+            subst_input = subst_output
+            for match in re.finditer(anchor_regexpr, subst_input):
+                expression, optional, anchor_name, accu_sep = match.group(1), match.group(2), match.group(3), match.group(4)
+                if anchor_name in accu:
+                    if anchor_name in input_params:
+                        return {'return':1, 'error':"Both input_params and accu contain '{}' anchor, ambiguous substitution".format(anchor_name)}
+                    else:
+                        # print("Substituting {} -> {} from accu".format(expression, accu_sep.join(accu[anchor_name])))
+                        accu_value_list = accu[anchor_name]
+                        if type(accu_value_list)!=list:
+                            accu_value_list = [ accu_value_list ]
+                        subst_output = subst_output.replace(expression, accu_sep.join(accu_value_list) )
+                elif anchor_name in input_params:
+                    # print("Substituting {} -> {} from input_params".format(expression, accu_sep.join(input_params[anchor_name])))
+                    subst_output = subst_output.replace(expression, accu_sep.join(input_params[anchor_name]) )
+                elif optional=='?':
+                    # print("Substituting optional {} -> ''".format(expression))
+                    subst_output = subst_output.replace(expression, '')
+                else:
+                    return {'return':1, 'error':"Neither input_params nor accu contain substitution for non-optional '{}' anchor".format(anchor_name)}
+
+                # print("input={}\noutput={}\n".format(subst_input, subst_output))
+
+            can_substitute  = bool( re.search(anchor_regexpr, subst_output) )
+            if interactive:
+                iteration += 1
+                print("Substitution iteration #{}".format(iteration))
+
+        if interactive:
+            print("The generated command:\n  {}".format(subst_output.replace(' --', ' \\\n    --')))
+
+        cmds.append( subst_output )
+
+    return { 'return': 0, 'cmds': cmds }
 
 
 def run(i):
@@ -202,13 +247,23 @@ def run(i):
 
     import os
 
+    interactive = i.get('out')=='con'
+
     i['action'] = 'gen'
+    i['out']    = ''
     r=ck.access( i )
     if r['return']>0: return r
+    cmds = r['cmds']
 
-    cmd = r['cmd']
+    for cmd in cmds:
+        if interactive:
+            print('='*80)
+            print("Running command:\n\t"+cmd)
+            print('')
+        os.system(cmd)
 
-    os.system(cmd)
+    if interactive:
+        print('='*80)
 
     return { 'return': 0 }
 
