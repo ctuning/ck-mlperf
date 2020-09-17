6137409ffb18da1dc2d68f8575f72fdcb348848b
Gavin Simpson <gavin.s.simpson@gmail.com>
2020-09-17 14:38:20 +0000

    changes to encorporate CK_LOADGEN_DATASET_SIZE

--- script/object-detection/schindler.py
+++ script/object-detection/schindler.py
@@ -10,13 +10,14 @@ import os
 
 def ck_preprocess(i):
     print('\n-=-=-=-=-= Generating a list of files to be processed...')
+    def has_env(var): return var in i['env']
     def my_env(var): return i['env'].get(var)
     def dep_env(dep, var): return i['deps'][dep]['dict']['env'].get(var)
     def has_dep_env(dep, var): return var in i['deps'][dep]['dict']['env']
 
     index_filename  = dep_env('dataset', 'CK_ENV_DATASET_OBJ_DETECTION_PREPROCESSED_SUBSET_FOF')
     source_dir      = dep_env('dataset', 'CK_ENV_DATASET_OBJ_DETECTION_PREPROCESSED_DIR')
-    image_count     = int(my_env('CK_BATCH_SIZE')) * int(my_env('CK_BATCH_COUNT'))
+    image_count     = int(my_env('CK_LOADGEN_DATASET_SIZE')) if has_env('CK_LOADGEN_DATASET_SIZE') else int(my_env('CK_BATCH_SIZE')) * int(my_env('CK_BATCH_COUNT'))
     images_offset   = int(my_env('CK_SKIP_IMAGES') or '0')
 
     all_index_lines = []
