bugfix: be silent for CK_VERBOSE=0

program/image-classification-tensorrt-loadgen-py/tensorrt_classify_loadgen.py
@@ -69,7 +69,9 @@ def load_query_samples(sample_indices):     # 0-based indices in our whole datas
 
         preprocessed_image_buffer[sample_index] = np.array(img)
         tick('l')
-    print('')
+
+    if VERBOSITY_LEVEL:
+        print('')
 
 
 def unload_query_samples(sample_indices):
@@ -78,7 +80,9 @@ def unload_query_samples(sample_indices):
 
     preprocessed_image_buffer = {}
     tick('U')
-    print('')
+
+    if VERBOSITY_LEVEL:
+        print('')
 
 
 def issue_queries(query_samples):