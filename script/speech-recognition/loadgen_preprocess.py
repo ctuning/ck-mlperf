17b8757b82541636f749949d93b480cac399a3da
Gavin Simpson <gavin.s.simpson@gmail.com>
2020-09-29 14:51:29 +0100

    RNN-T loadgen program tidy-up.

--- script/speech-recognition/loadgen_preprocess.py
+++ script/speech-recognition/loadgen_preprocess.py
@@ -45,7 +45,7 @@ def user_conf_and_audit_config(i):
 
     model_name = "rnnt"
     print('\n-=-=-=-=-= Generating user.conf for model "{}" ...'.format(model_name))
-    scenario            = "-"
+    scenario            = env['CK_LOADGEN_SCENARIO']
     user_conf_rel_path  = env['CK_LOADGEN_USER_CONF']
     user_conf           = []
     env_to_conf         = {
@@ -90,42 +90,7 @@ def user_conf_and_audit_config(i):
     return {'return':0}
 
 
-def schindler(i):
-    print('\n-=-=-=-=-= Generating a list of files to be processed...')
-    def has_env(var): return var in i['env']
-    def my_env(var): return i['env'].get(var)
-    def dep_env(dep, var): return i['deps'][dep]['dict']['env'].get(var)
-    def has_dep_env(dep, var): return var in i['deps'][dep]['dict']['env']
-
-    image_list_filename = dep_env('images', 'CK_ENV_DATASET_IMAGENET_PREPROCESSED_SUBSET_FOF')
-    source_dir          = dep_env('images', 'CK_ENV_DATASET_IMAGENET_PREPROCESSED_DIR')
-    preprocessed_ext    = dep_env('images', 'CK_ENV_DATASET_IMAGENET_PREPROCESSED_NEW_EXTENSION')
-
-    image_count         = int(my_env('CK_LOADGEN_DATASET_SIZE')) if has_env('CK_LOADGEN_DATASET_SIZE') else int(my_env('CK_BATCH_SIZE')) * int(my_env('CK_BATCH_COUNT'))
-    images_offset       = int(my_env('CK_SKIP_IMAGES') or '0')
-
-    sorted_filenames    = [filename for filename in sorted(os.listdir(source_dir)) if filename.lower().endswith('.' + preprocessed_ext) ]
-
-    selected_filenames  = sorted_filenames[images_offset:images_offset+image_count] if image_count else sorted_filenames[images_offset:]
-
-    selected_var_paths  = [ os.path.join("$<<CK_ENV_DATASET_IMAGENET_PREPROCESSED_DIR>>$", filename) for filename in selected_filenames ]
-
-    with open(image_list_filename, 'w') as f:
-        for filename in selected_filenames:
-            f.write(filename + '\n')
-
-    print('=-=-=-=-=- done.\n')
-
-    return {
-        'return': 0,
-        'new_env': {},
-        'run_input_files': [ '$<<>>$' + image_list_filename ] + selected_var_paths,
-        'run_output_files': [],
-    }
-
-
-# This preprocessing subroutine is a combination of several sequential processes,
-# but the only non-trivial output is the output of schindler(), so we call it last:
+# This preprocessing subroutine is a combination of several sequential processes.
 #
 def ck_preprocess(i):
     env=i['env']
@@ -138,8 +103,19 @@ def ck_preprocess(i):
     if env.get('CK_MLPERF_PRE_USER_CONF_AND_AUDIT_CONFIG','').lower() in ('yes', 'on', 'true', '1'):
         ret_dict = user_conf_and_audit_config(i)
 
-    if env.get('CK_MLPERF_PRE_SCHINDLER','').lower() in ('yes', 'on', 'true', '1'):
-        ret_dict = schindler(i)
+    def dep_env(dep, var): return i['deps'][dep]['dict']['env'].get(var)
+
+    inferencepath = dep_env('mlperf-inference', 'CK_ENV_MLPERF_INFERENCE')
+
+    try:
+      pythonpath = os.environ['PYTHONPATH'] + ":"
+    except KeyError:
+      pythonpath = ""
+
+    os.environ['PYTHONPATH'] = pythonpath + \
+                                 os.path.join(inferencepath,"speech_recognition/rnnt") + ":" + \
+                                 os.path.join(inferencepath,"speech_recognition/rnnt/pytorch")
+
 
     return ret_dict
 
