2654ba2d6d8452b2aae5cba33486c5eaee427df9
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-10-09 15:11:39 +0100

    perform patching of the original BERT code on the fly and cache the patched code in program's tmp/

--- program/bert-reference-loadgen-py/setup_and_run_bert.sh
+++ program/bert-reference-loadgen-py/setup_and_run_bert.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+## Usually program's tmp/
+#
+CWD=`pwd`
+
 export BERT_REF_ROOT=${CK_ENV_MLPERF_INFERENCE}/language/bert
-export BERT_BUILD=./build
+export BERT_BUILD=${CWD}/build
 export BERT_BUILD_DATA=${BERT_BUILD}/data
-export PYTHONPATH=${PYTHONPATH}:${BERT_REF_ROOT}/DeepLearningExamples/TensorFlow/LanguageModeling/BERT
+
+## CWD takes priority over the original code, because we patch some of the original files and retain them in CWD:
+#
+export PYTHONPATH=${PYTHONPATH}:${CWD}:${BERT_REF_ROOT}:${BERT_REF_ROOT}/DeepLearningExamples/TensorFlow/LanguageModeling/BERT
 
 
 if [ ! -e "$BERT_BUILD" ]; then
@@ -22,7 +29,6 @@ if [ ! -e "$BERT_BUILD" ]; then
     fi
 fi
 
-CWD=`pwd`
 
 cp ${BERT_REF_ROOT}/bert_config.json $CWD
 cp ${BERT_REF_ROOT}/user.conf $CWD
@@ -30,14 +36,25 @@ cp ${BERT_REF_ROOT}/user.conf $CWD
 make -f ${BERT_REF_ROOT}/Makefile download_data
 make -f ${BERT_REF_ROOT}/Makefile download_model
 
-cp ${BERT_REF_ROOT}/create_squad_data.py ${BERT_REF_ROOT}/DeepLearningExamples/TensorFlow/LanguageModeling/BERT/utils/create_squad_data.py
-
-sed -i'' 's/ accuracy-squad.py/ .\/accuracy-squad.py/g' ${BERT_REF_ROOT}/run.py
-
-rm -rf build/logs build/result bert_code
-
+## BERT scripts expect a specific structure of build/ directory which we then bend to suit our needs:
+#
+rm -rf build/logs build/result bert_code utils
 ln -s $CWD build/logs
 ln -s $CWD build/result
 ln -s ${BERT_REF_ROOT} bert_code
 
-$CK_ENV_COMPILER_PYTHON_FILE ${BERT_REF_ROOT}/run.py --mlperf_conf=${CK_ENV_MLPERF_INFERENCE}/mlperf.conf --user_conf=${BERT_REF_ROOT}/user.conf --backend=${CK_BERT_BACKEND} --scenario=${CK_LOADGEN_SCENARIO} $CK_LOADGEN_MODE_STRING
+## Patching some of BERT's scripts:
+#
+sed 's/ accuracy-squad.py/ .\/accuracy-squad.py/g' ${BERT_REF_ROOT}/run.py >${CWD}/run.py
+
+## Adding one line to a Python script without disrupting the indentation structure:
+#
+EXTRA_LINE="if os.environ.get('CK_BERT_TRANSFORMERS_OVERRIDE','no').lower() in ('yes','on','true','1'): self.model.bert.set_weights_split()"
+sed "/^([\ \t]*)self.model.load_state_dict/a \1${EXTRA_LINE}" ${BERT_REF_ROOT}/pytorch_SUT.py >${CWD}/pytorch_SUT.py
+
+mkdir ${CWD}/utils
+cp ${BERT_REF_ROOT}/create_squad_data.py ${CWD}/utils/create_squad_data.py
+
+## Run the patched version of run.py:
+#
+$CK_ENV_COMPILER_PYTHON_FILE ${CWD}/run.py --mlperf_conf=${CK_ENV_MLPERF_INFERENCE}/mlperf.conf --user_conf=${BERT_REF_ROOT}/user.conf --backend=${CK_BERT_BACKEND} --scenario=${CK_LOADGEN_SCENARIO} $CK_LOADGEN_MODE_STRING
