08dcfe122a2ba9b8ae6208be8c51b8088f909536
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-10-09 16:56:57 +0100

    restructured build/ directory so that the build/data part could be shared among users of the same host

--- program/bert-reference-loadgen-py/setup_and_run_bert.sh
+++ program/bert-reference-loadgen-py/setup_and_run_bert.sh
@@ -16,17 +16,21 @@ export PYTHONPATH=${PYTHONPATH}:${CWD}:${BERT_REF_ROOT}:${BERT_REF_ROOT}/DeepLea
 if [ ! -e "$BERT_BUILD" ]; then
     echo "Build directory does not exist yet"
 
-    if [ -n "$CK_BERT_REF_BUILD_DIR" ]; then
-        echo "Given the following path to be used as build directory: $CK_BERT_REF_BUILD_DIR"
-        if [ ! -e "$CK_BERT_REF_BUILD_DIR" ]; then
+    mkdir $BERT_BUILD
+
+    if [ -n "$CK_BERT_DATA" ]; then
+        echo "Given the following path to be used as build directory: $CK_BERT_DATA"
+        if [ ! -e "$CK_BERT_DATA" ]; then
             echo "It did not exist, creating it..."
-            mkdir $CK_BERT_REF_BUILD_DIR
+            mkdir $CK_BERT_DATA
         fi
-        ln -s $CK_BERT_REF_BUILD_DIR $BERT_BUILD
+        ln -s $CK_BERT_DATA $BERT_BUILD/data
     else
         echo "Not given a path for build directory, creating $BERT_BUILD ... Beware, it may take a lot of space!"
-        mkdir $BERT_BUILD
+        mkdir $BERT_BUILD/data
     fi
+    ln -s $CWD ${BERT_BUILD}/logs
+    ln -s $CWD ${BERT_BUILD}/result
 fi
 
 
@@ -37,12 +41,11 @@ git -C $BERT_REF_ROOT submodule update --init DeepLearningExamples
 make -f ${BERT_REF_ROOT}/Makefile download_data
 make -f ${BERT_REF_ROOT}/Makefile download_model
 
-## BERT scripts expect a specific structure of build/ directory which we then bend to suit our needs:
+
+## A link for our convenience (not used by the scripts) :
 #
-rm -rf build/logs build/result bert_code utils
-ln -s $CWD build/logs
-ln -s $CWD build/result
-ln -s ${BERT_REF_ROOT} bert_code
+rm -f ${CWD}/bert_code
+ln -s ${BERT_REF_ROOT} ${CWD}/bert_code
 
 ## Patching some of BERT's scripts:
 #
@@ -53,6 +56,7 @@ sed 's/ accuracy-squad.py/ .\/accuracy-squad.py/g' ${BERT_REF_ROOT}/run.py >${CW
 EXTRA_LINE="if os.environ.get('CK_BERT_TRANSFORMERS_OVERRIDE','no').lower() in ('yes','on','true','1'): self.model.bert.set_weights_split()"
 sed "/^([\ \t]*)self.model.load_state_dict/a \1${EXTRA_LINE}" ${BERT_REF_ROOT}/pytorch_SUT.py >${CWD}/pytorch_SUT.py
 
+rm -rf ${CWD}/utils
 mkdir ${CWD}/utils
 cp ${BERT_REF_ROOT}/create_squad_data.py ${CWD}/utils/create_squad_data.py
 
